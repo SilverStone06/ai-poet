c488d67dd6fa2f64f49e8473dc66d27e109044ae
SilverStone06 <84123429+SilverStone06@users.noreply.github.com>
2024-02-07 06:09:29 +0000

    수정

--- Langchain_Practice.py
+++ Langchain_Practice.py
@@ -1,10 +1,9 @@
-from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 
 #export OPENAI_API_KEY = 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
 
 def poet_content(content) :
-    load_dotenv()
+    
     chat_model = ChatOpenAI(api_key = "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")  # api_key 매개변수로 전달
 
     result = chat_model.invoke(content + "에 대한 시를 써줘.")
