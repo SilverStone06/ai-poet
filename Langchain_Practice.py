e098ad0e11901e4d41612e9a5a3fb41749d26aaf
SilverStone06 <84123429+SilverStone06@users.noreply.github.com>
2024-02-14 05:20:03 +0000

    간소화ver2

--- Langchain_Practice.py
+++ Langchain_Practice.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 
 def poem_content(content) :
     
-    chat_model = ChatOpenAI(api_key = "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")  # api_key 매개변수로 전달
+    chat_model = ChatOpenAI()  # api_key 매개변수로 전달
 
     result = chat_model.invoke(content + "에 대한 시를 써줘.")
     return result
