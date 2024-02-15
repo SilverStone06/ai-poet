4cd19fbb505f8fa27c5ceb46fed7ce6a19ed83b0
SilverStone06 <84123429+SilverStone06@users.noreply.github.com>
2024-02-15 05:00:41 +0000

    수익화

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from Langchain_Practice import poem_content
+from streamlit_extras.buy_me_a_coffee import button
 
+def BMC():
+    button(username="fake-username", floating=False, width=221)
+
+BMC()
 st.title('인공지능 시인')
 title = st.text_input('시의 주제를 입력해 주세요.')
 if st.button('시 작성 요청하기'):
